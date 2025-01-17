b6258c27ae81c4e60d293ffb8fe4c2a18a8aef0e
Hollman Rojas <hollmanerojas@gmail.com>
2025-01-16 23:02:07 -0500

    Feature/validations into templates sign-in and sign-out

--- ecommercenp/settings.py
+++ ecommercenp/settings.py
@@ -112,13 +112,18 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es'
 
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
 USE_TZ = True
+
+IDIOMAS = [
+    ('en', 'Inglés'),
+    ('es', 'Español'),
+]
 
 
 # Static files (CSS, JavaScript, Images)
